1a072cb9af99586bf005a80f37285e48210f7aef
Anurag <anuragrajanp@gmail.com>
2024-10-14 10:30:20 +0530

    fix some errors

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
 import blog, { ga, redirects } from "https://deno.land/x/blog/blog.tsx";
-import { unocss_opts } from "./unocss.ts";
+// import { unocss_opts } from "./unocss.ts";
 
 blog({
 	author: "Anurag",
@@ -11,19 +11,18 @@ blog({
 	links: [
 		{ title: "Email", url: "mailto:anuragrajanp@gmail.com" },
 		{ title: "GitHub", url: "https://github.com/anuragpoolakkal" },
-		// { title: "Twitter", url: "https://twitter.com/denobot" },
 	],
 	lang: "en",
 	// localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
 	dateFormat: (date) => new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
 	middlewares: [
-		ga("UA-XXXXXXXX-X"),
+		// ga("UA-XXXXXXXX-X"),
 		redirects({
-			"/foo": "/my_post",
+			// "/foo": "/my_post",
 			// you can skip leading slashes too
-			bar: "my_post2",
+			hello_world: "hello_world",
 		}),
 	],
-	unocss: unocss_opts, // check https://github.com/unocss/unocss
+	// unocss: unocss_opts, // check https://github.com/unocss/unocss
 	favicon: "files/circle.svg",
 });
