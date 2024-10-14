6efa5570e0e5eab6636ef5d64b4c6c9621adca51
Anurag <anuragrajanp@gmail.com>
2024-10-14 10:53:05 +0530

    post: hello deno

--- main.tsx
+++ main.tsx
@@ -1,12 +1,12 @@
-import blog, { ga, redirects } from "https://deno.land/x/blog/blog.tsx";
+import blog from "blog"; // { ga, redirects, h }
 // import { unocss_opts } from "./unocss.ts";
 
 blog({
 	author: "Anurag",
 	title: "Anurag",
 	// description: "The blog description.",
-	avatar: "files/circle.svg",
-	avatarClass: "rounded-full",
+	// avatar: "files/circle.svg",
+	// avatarClass: "rounded-full",
 	showHeaderOnPostPage: false,
 	links: [
 		{ title: "Email", url: "mailto:anuragrajanp@gmail.com" },
@@ -14,15 +14,16 @@ blog({
 	],
 	lang: "en",
 	// localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
-	dateFormat: (date) => new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
-	middlewares: [
-		// ga("UA-XXXXXXXX-X"),
-		redirects({
-			// "/foo": "/my_post",
-			// you can skip leading slashes too
-			hello_world: "hello_world",
-		}),
-	],
+	dateFormat: (date: number | Intl.Formattable | undefined) =>
+		new Intl.DateTimeFormat("en-GB", { dateStyle: "long" }).format(date),
+	// middlewares: [
+	// 	// ga("UA-XXXXXXXX-X"),
+	// 	redirects({
+	// 		// "/foo": "/my_post",
+	// 		// you can skip leading slashes too
+	// 		hello_world: "hello_world",
+	// 	}),
+	// ],
 	// unocss: unocss_opts, // check https://github.com/unocss/unocss
 	favicon: "files/circle.svg",
 });
--- posts/hello_world.md
+++ posts/hello_world.md
@@ -1,6 +1,6 @@
 ---
-title: Hello world!
+title: Hello Deno!
 publish_date: 2024-10-14
 ---
 
-This is my first blog post!
+This is my first blog using Deno.
